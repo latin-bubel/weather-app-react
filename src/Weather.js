77d762fce1f8e531f606335aca3c8db161070cd5
latin bubel <esterapietkiewicz0110@gmail.com>
2022-11-21 12:13:15 +0100

    Changes
    Included Bootstrap,
    Inlcuded CSS

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
 import WeatherInformation from "./WeatherInformation";
-import DateBase from "./DateBase";
 
 export default function Weather(props) {
   const [weather, setWeather] = useState({ ready: false });
--- src/WeatherInformation.js
+++ src/WeatherInformation.js
@@ -1,5 +1,7 @@
 import React from "react";
 import DateBase from "./DateBase";
+import WeatherTemperature from "./WeatherTemperature";
+import "./WeatherInformation.css";
 
 export default function WeatherInformation(props) {
   return (
@@ -12,9 +14,17 @@ export default function WeatherInformation(props) {
         <li>{props.data.description}</li>
       </ul>
       <div class="row">
-        <div className="weather-climate col-6">
+        <div class="col-6">
+          <div className="weather-icon d-inline-flex p-2">
+            <img src={props.data.icon} alt="Cloud" size={30} />
+          </div>
+          <div className="weather-temperature col-4 float-start inline ">
+            <WeatherTemperature celsius={props.data.temperature} />
+          </div>
+        </div>
+        <div className="weather-description col-6">
           <ul>
-            <li>Humidity: {props.data.humidity}%</li>
+            <li>Humidity: {props.data.humidity} %</li>
             <li>Wind: {props.data.wind} km/h</li>
           </ul>
         </div>
